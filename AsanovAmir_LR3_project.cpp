6a3dc99a174e99f2bda752356242965ff3ae18a8
AmirAsanov <genishbekovamir7@gmail.com>
2025-03-17 13:45:03 +0300

    ready function for find max number

---
+++ AsanovAmir_LR3_project.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[5] = {2,1,3,4,5};
+    int max = array[0];
+
+    for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++) {
+        if(array[i] > max) {
+            max = array[i];
+        }
+
+    };
+
+    cout << max;
+
+    return 0;
+}
